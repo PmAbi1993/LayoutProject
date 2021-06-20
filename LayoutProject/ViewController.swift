7a88edeb8c5bffa8c3b90d909bbaf5fb86981c13
PmAbi1993 <ylikeme4ever@gmail.com>
2021-06-20 16:13:25 +0530

    Basic view layouts added

--- LayoutProject/ViewController.swift
+++ LayoutProject/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 import Layout
 
 class ViewController: UIViewController {
+    var redView: UIView = {
+        let view: UIView = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    var blueView: UIView = {
+        let view: UIView = UIView()
+        view.backgroundColor = .blue
+
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.layout([.left, .right], parentView: view)
+        redView.layout(with: [.centerX(to: view, constant: 0),
+                              .centerY(to: view, constant: 0),
+                              .height(constant: 100),
+                              .width(constant: 100)], parentView: view)
+        blueView.layout(with: [.topTo(redView, constant: 8),
+                               .centerX(to: view, constant: 0),
+                               .width(constant: 100),
+                               .height(constant: 100)], parentView: view)
     }
 }
