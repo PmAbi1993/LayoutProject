ef88cfc8e212b65fd63f7871135979fad4b99ff7
PmAbi1993 <ylikeme4ever@gmail.com>
2021-06-20 19:37:26 +0530

    Added click listeners

--- LayoutProject/ViewController.swift
+++ LayoutProject/ViewController.swift
@@ -11,9 +11,10 @@ import Layout
 class ViewController: UIViewController {
     lazy var tableView: UITableView = {
         let tableView: UITableView = UITableView()
-        tableView.registerCells([UITableViewCell.self])
+        tableView.registerCells([TableCell.self])
         tableView.dataSource = self
         tableView.delegate = self
+//        tableView.allowsSelection = false
         return tableView
     }()
 
@@ -26,11 +27,56 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate,
                            UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 100
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequedCell(type: UITableViewCell.self)
-        cell.textLabel?.text = "bvc"
+        let cell: TableCell = tableView.dequedCell(type: TableCell.self)
+        cell.label.text = "This is cell: \(indexPath.row)"
         return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 96
+    }
+}
+
+class TableCell: UITableViewCell {
+    var cellImageView: UIImageView = {
+        let view: UIImageView = UIImageView()
+        view.backgroundColor = .red
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    var label: UILabel = {
+        let view: UILabel = UILabel()
+        view.numberOfLines = 0
+        view.backgroundColor = .green
+        return view
+    }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        loadView()
+        setClickListners()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func loadView() {
+        cellImageView.layout(with: [.top(8),
+                                    .left(8),
+                                    .width(constant: 80),
+                                    .height(constant: 80)],
+                             parentView: contentView)
+        label.layout(with: [.rightTo(cellImageView,
+                                     constant: 8),
+                            .right(-8),
+                            .top(8),
+                            .bottom(-8)],
+                     parentView: contentView)
+    }
+    
+    func setClickListners() {
+        cellImageView.onClick {
+            print("Clicked in the image")
+        }
     }
 }
